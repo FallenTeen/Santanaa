924aa6b0552f0b7b3b56420b25af824911847f37
febrinurhidayat <febrinurhidayat35364@gmail.com>
2024-07-04 01:20:55 +0700

    pp

--- resources/views/livewire/booking/create-booking.blade.php
+++ resources/views/livewire/booking/create-booking.blade.php
@@ -89,8 +89,8 @@
                     </div>
                 @endforeach
             </div>
-            <button type="button" wire:click="kembali" class="inline-block px-4 py-2 mt-4 bg-gray-300 text-gray-700 rounded hover:bg-gray-400">Back</button>
             <button type="button" wire:click="nextStep" class="inline-block px-4 py-2 ml-4 bg-indigo-500 text-white rounded hover:bg-indigo-600">Next</button>
+            <button type="button" wire:click="kembali" class="inline-block px-4 py-2 mt-4 bg-gray-300 text-gray-700 rounded hover:bg-gray-400">Back</button>
         </form>
     @elseif($currentStep == 4)
         {{-- Step 4: Review & Submit --}}
